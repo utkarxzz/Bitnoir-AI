f7312ade4d9f9ef4050ebc9ff9307f546a1a9562
Utkarsh Tiwari <utkarxxtiwari@gmail.com>
2026-08-13 11:44:40 -0400

    Add files via upload

---
+++ actions/presentations/__init__.py
@@ -0,0 +1,10 @@
+"""Reusable building blocks for JARVIS presentation jobs."""
+
+from .models import PresentationRequest, PresentationResult, SourceBundle, SourceRecord
+
+__all__ = [
+    "PresentationRequest",
+    "PresentationResult",
+    "SourceBundle",
+    "SourceRecord",
+]
